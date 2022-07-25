d3da917e19be2b5d2aeb37ec0e11d5ab629f79b1
Lennard Sy <l.samuel1996@gmail.com>
2022-07-25 19:54:41 +0200

    platform image and code
    Added the platform image (cucumber) and added the code, so that the player can jump on the plattforms

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #import libraries
 # 
+from platform import platform
 import pygame
+import random
 
 #initialize pygame
 pygame.init()
@@ -24,6 +26,8 @@ FPS = 60
 
 #GameVariables
 GRAVITY = 1
+MAX_PLATFORMS = 10 
+
 
 #define colours
 WHITE = (255,255,255)
@@ -31,8 +35,11 @@ WHITE = (255,255,255)
 #load images
 player_image = pygame.image.load("Assets/Doodler_char.png").convert_alpha()
 bg_image = pygame.image.load("Assets/el-capitan.png").convert_alpha()
+platform_image = pygame.image.load("Assets/cucumber_platform.png").convert_alpha()
 
-#player class
+
+
+#player class 
 class Player(): 
     
     def __init__(self,x,y):
@@ -92,6 +99,19 @@ class Player():
         if self.rect.right + dx > SCREEN_WIDTH:
             dx = SCREEN_WIDTH - self.rect.right
 
+        #check collision with platforms
+        for platform in platform_group:
+            #collision in the y direction
+            if platform.rect.colliderect(self.rect.x,self.rect.y + dy, self.width, self.height):
+                #check if above platfrom
+                if self.rect.bottom < platform.rect.centery: 
+                    if self.vel_y > 0:
+                        self.rect.bottom = platform.rect.top
+                        dy = 0 
+                        self.vel_y = -20
+                
+                
+                
         # check collision with ground
         if self.rect.bottom + dy > SCREEN_HIGHT:
             dy = 0 
@@ -111,9 +131,32 @@ class Player():
         #pygame.draw.rect(screen, WHITE,self.rect, 2)
 
 
+#we are using a sprite class for more functionality
+class Platform(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, width):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(platform_image,(width,10))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+#player instance
 # sets Player coordinates in middle bottom part of the screen
 jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HIGHT - 150 )
 
+#create sprite groups
+platform_group = pygame.sprite.Group()
+
+#create temporary platfroms
+#for ramdonmization we import the module "random"
+for p in range(MAX_PLATFORMS):
+    p_width = random.randint(40,60)
+    p_x = random.randint(0, SCREEN_WIDTH - p_width)
+    p_y = p* random.randint(80,120)
+    platform = Platform(p_x,p_y,p_width)
+    platform_group.add(platform)
 
 #game loop
 # game runs as long as run = true
@@ -132,6 +175,7 @@ while run :
     screen.blit(bg_image, (-100,0))
 
     #draw sprites 
+    platform_group.draw(screen)
     jumpy.draw()
 
 
