aab63c616be72d1952a40de591cb6feba022097a
Lennard Sy <l.samuel1996@gmail.com>
2022-07-27 21:18:49 +0200

    Game_over, screen_text,fade_transition,comments
    In diesem update habe ich die funktion herausgenommen, dass player von dem boden bounced, stattdessen ist sobald der spieler den unteren screen verlässt, eine game over situation. Weiterhin habe ich nach der game over situation einen on screen text eingefügt und die funktionalität, dass nach pressen des Space bars das spiel wieder von vorne anfängt. Zuletzt habe ich eine fade transition eingebaut, die zum einen den Text besser lesen lässt und zum anderen unterstreicht dass das spiel verloren ist.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #import libraries
 # 
 from platform import platform
+
+from sqlalchemy import false, true
 import pygame
 import random
 
@@ -41,13 +43,35 @@ scroll = 0
 # sets the background scroll speed
 bg_scroll = 0
 
+game_over = False
+
+score = 0
+
+#variable for screen transition
+fade_counter = 0 
+
+
+
 #define colours
 WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+#define font for on screen text
+font_small = pygame.font.SysFont("Lucida Sans", 20)
+
+font_big = pygame.font.SysFont("Lucida Sans", 24)
 
 #load images
 player_image = pygame.image.load("Assets/Doodler_char.png").convert_alpha()
 bg_image = pygame.image.load("Assets/el-capitan_croped.png").convert_alpha()
 platform_image = pygame.image.load("Assets/cucumber_platform.png").convert_alpha()
+
+
+#function for outputting text onto the screen
+#
+def draw_text(text,font,text_col,x,y):
+    img = font.render(text,True,text_col)
+    screen.blit(img,(x,y))
 
 
 #function for drawing the background
@@ -86,6 +110,8 @@ class Player():
         dy = 0
         #scroll variable is resetet every time move is called
         scroll = 0
+
+
         # which keypresses are present
         key = pygame.key.get_pressed()
         
@@ -133,13 +159,13 @@ class Player():
                         dy = 0 
                         self.vel_y = -20
                 
-                
-                
+        # we dont need this anymore        
+        '''        
         # check collision with ground
         if self.rect.bottom + dy > SCREEN_HEIGHT:
             dy = 0 
             self.vel_y = -20
-
+        '''
 
         # check if the player has bounced to the top of the screen
         if self.rect.top <= SCROLL_THRESH:
@@ -214,7 +240,7 @@ for p in range(MAX_PLATFORMS):
 platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT -50, 100 )
 platform_group.add(platform)
 #game loop
-# game runs as long as run = true
+# game runs as long as run = True
 run = True
 
 while run : 
@@ -222,47 +248,91 @@ while run :
     #ensures that gameplay is 60 frames per second
     clock.tick(FPS)
 
-    #enables movement of the player
-    scroll = jumpy.move()
-    
 
-    #draw backgound image
-    bg_scroll += scroll
-    #if background scroll reaches top of second bg image it is reset to 0 -> endless scroll
-    #image height is 600 pixels
-    if bg_scroll >= 600: 
-        bg_scroll = 0
-
-    draw_bg(bg_scroll)
-
-
-    '''
-    #draw temporary scrollthreshhold
-    pygame.draw.line(screen,WHITE,(0,SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
-    '''
-    #generate platforms
-    if len(platform_group) < MAX_PLATFORMS:
-        p_width = random.randint(40,60)
-        p_x = random.randint(0, SCREEN_WIDTH - p_width)
-        p_y = platform.rect.y - random.randint(80,120)
-        platform = Platform(p_x,p_y,p_width)
-        platform_group.add(platform)
+    if game_over == False:
+        #enables movement of the player
+        scroll = jumpy.move()
         
 
+        #draw backgound image
+        bg_scroll += scroll
+        #if background scroll reaches top of second bg image it is reset to 0 -> endless scroll
+        #image height is 600 pixels
+        if bg_scroll >= 600: 
+            bg_scroll = 0
 
-    #update platforms
-    platform_group.update(scroll)
+        draw_bg(bg_scroll)
 
-    #draw sprites 
-    #draw method of platform_groups comes with sprite groups
-    platform_group.draw(screen)
-    jumpy.draw()
+
+        '''
+        #draw temporary scrollthreshhold from which screen starts scrolling
+        pygame.draw.line(screen,WHITE,(0,SCROLL_THRESH), (SCREEN_WIDTH, SCROLL_THRESH))
+        '''
+        #generate platforms
+        if len(platform_group) < MAX_PLATFORMS:
+            p_width = random.randint(40,60)
+            p_x = random.randint(0, SCREEN_WIDTH - p_width)
+            p_y = platform.rect.y - random.randint(80,120)
+            platform = Platform(p_x,p_y,p_width)
+            platform_group.add(platform)
+            
+
+
+        #update platforms
+        platform_group.update(scroll)
+
+        #draw sprites 
+        #draw method of platform_groups comes with sprite groups
+        platform_group.draw(screen)
+        jumpy.draw()
+
+
+        #check if game over
+        # if the top of the player instance is higher than the screenheight (600p) than player has fallen off the screen
+
+        if jumpy.rect.top > SCREEN_HEIGHT: 
+            game_over = True
+    
+    else:
+        if fade_counter < SCREEN_WIDTH:
+            fade_counter += 5
+            # here we make a fancy transition into black screen with 3 Black bars coming in from left and right respectively
+            for y in range(0,6,2):
+                pygame.draw.rect(screen,BLACK,(0,y * 100,fade_counter, SCREEN_HEIGHT / 6))
+                pygame.draw.rect(screen,BLACK,(SCREEN_WIDTH - fade_counter,(y+1) * 100 ,SCREEN_WIDTH, SCREEN_HEIGHT/6))
+                
+
+        #if game_over == True it outputs a text saying "GAME OVER!"
+        draw_text("GAME OVER!",font_big,WHITE,130,200)
+        draw_text("SCORE:" + str(score),font_big, WHITE,130,150)
+        draw_text("PRESS SPACE TO PLAY AGAIN",font_big,WHITE,40,300)
+        key = pygame.key.get_pressed()
+        
+        if key[pygame.K_SPACE]:
+			#reset variables
+            game_over = False
+            #print(game_over)
+            score = 0
+            scroll = 0
+            fade_counter = 0
+			#reposition jumpy
+            jumpy.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+			#reset platforms
+            platform_group.empty()
+			#create starting platform
+            platform = Platform(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 50, 100)
+            platform_group.add(platform)
+            
 
 
     #event handler (python function)
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             run = False
+            
+
+
+
 
 
     #update display window
